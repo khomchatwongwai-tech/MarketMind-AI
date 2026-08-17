2474b7b2a8248846bae721a8bcdb33b88794e4c2
khomchat wongwai <khomchatwongwai@khomchats-MacBook-Air.local>
2026-08-16 21:17:02 -0400

    add Capacitor iOS and Android apps

---
+++ android/app/src/main/java/com/marketmindai/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.marketmindai.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marketmindai.app',
+  appName: 'MarketMind AI',
+  webDir: 'dist'
+};
+
+export default config;
